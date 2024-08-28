a43b84b83074e63dacb50daa83b6bae5d03f0652
Kevvinnn-s <kevinsandjaya898@gmail.com>
2024-08-29 00:06:56 +0700

    updates

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
 // ini javascript
+
+function formValidation() {
+  // ambil DOM
+    let name = document.getElementById('name').value;
+    // Kondisi pemisah
+    if (name == '') {
+        alert('Form Mohon Diisi');
+        document.getElementById('result').innerHTML = '-';
+    } else {
+        document.getElementById('result').innerText = name;
+    }
+    console.log(name);
+
+    let TanggalLahir = document.getElementById('date').value;
+    if(TanggalLahir == ''){
+      alert('Tanggal Mohon di Isi');
+      document.getElementById('result1').innerHTML ='-'
+    } else {
+      document.getElementById('result1').innerHTML = '';
+    }
+}
